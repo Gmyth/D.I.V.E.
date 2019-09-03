768a5d9e4092cc3199402bdbda2a9dc6add906a3
Zheyu Zhang <eric.zhangzheyu@gmail.com>
2019-09-03 13:03:53 -0400

    Fixed AttributeSetDrawer

--- src/Cyberpunk2078/Assets/Scripts/Attribute/AttributeSet.cs
+++ src/Cyberpunk2078/Assets/Scripts/Attribute/AttributeSet.cs
@@ -51,6 +51,7 @@ public class EventOnAttributeChange : UnityEvent<AttributeType, float, float> { 
 
 
     [SerializeField] private Attribute[] serializedAttributes;
+
     private Dictionary<AttributeType, float> attributes = new Dictionary<AttributeType, float>();
 
 
