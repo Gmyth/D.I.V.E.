38381b026f4c07d111cfd6b27473a3c12f2349c3
Yiyan Wang (RIT Student) <yw2267@ad.rit.edu>
2019-09-03 12:48:07 -0400

    saving changes

---
+++ src/Cyberpunk2078/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using System;
+
+
+public class DialogueManager: Singleton<DialogueManager>
+{   
+    private DialogueData[] dialogues;
+
+    public void InitDialogue()
+    {
+        string path = Application.dataPath + "/StreamingAssets" + "/Dia.json";
+        string jsonString = File.ReadAllText(path);
+        string fixedJsonString = JsonHelper.fixJson(jsonString);
+        dialogues = JsonHelper.FromJson<DialogueData>(fixedJsonString);
+        Debug.Log(dialogues);
+    }
+}
+
+[Serializable]
+public class DialogueData
+{
+    public int ID;
+    public string Type;
+    public string Actor;
+    public string Image;
+    public string Text;
+    public string Sound;
+    public string Effect;
+    public string Chapter;
+    public string Scene;
+
+}
+
