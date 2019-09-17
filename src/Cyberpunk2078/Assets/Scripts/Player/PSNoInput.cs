7616c91380f5f207be0b2e878de2d66885a5b8bf
Yiyan Wang <robinoneword@outlook.com>
2019-09-17 12:51:05 -0400

    Add PSNoInput
    Nin

---
+++ src/Cyberpunk2078/Assets/Scripts/Player/PSNoInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "PS_No_Input", menuName = "Player State/No Input")]
+public class PSNoInput : PlayerState
+{
+    public TimelineManager currentTimeline;
+
+    public override int Update()
+    {
+        if (isGrounded())
+        {
+            Rigidbody2D rb2d = playerCharacter.gameObject.GetComponent<Rigidbody2D>();
+            if (rb2d.velocity == Vector2.zero)
+            {
+                //currentTimeline.OnTimelineStart();
+                Debug.Log(LogUtility.MakeLogString("PSNoInput", "Timeline Start"));
+            }
+        }
+
+        return Index;
+    }
+
+
+    public override void OnStateEnter()
+    {
+        //kill all speed
+        Rigidbody2D rb2d = playerCharacter.gameObject.GetComponent<Rigidbody2D>();
+        rb2d.velocity = Vector2.zero;
+
+      
+    }
+
+
+}
