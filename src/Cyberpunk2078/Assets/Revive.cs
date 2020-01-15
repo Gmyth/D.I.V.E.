044fb912c3b88700d91cee07b23f848a8a25f3e7
robinsetsuna <robinoneword@outlook.com>
2020-01-15 14:46:52 -0500

    BossRoom

---
+++ src/Cyberpunk2078/Assets/Revive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revive : MonoBehaviour
+{
+    public float RecoverTime = 2.0f;
+    public Drone[] drone;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator Recover()
+    {
+        yield return new WaitForSeconds(RecoverTime);
+        gameObject.SetActive(true);
+    }
+}
