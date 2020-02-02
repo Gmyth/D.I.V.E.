aa6a6d9b64e36149922464b3879e92e3a06ec3d9
Gmyth <332087251@qq.com>
2020-02-01 22:11:39 -0500

    laser light under constuction

--- src/Cyberpunk2078/Assets/Scripts/VFX/LaserLight.cs
+++ src/Cyberpunk2078/Assets/Scripts/VFX/LaserLight.cs
@@ -8,69 +8,78 @@ public class LaserLight : MonoBehaviour
     // Start is called before the first frame update
     
     private float MoveAngle;
-    private float bpm = 120;  //TODO: Audio
-    private float idleInterval = 0.2f;
+    private float bpm = 80;  //TODO: Audio
+    //private float idleInterval = Random.Range(0.05f,0.2f);
     
     private bool flash;
     private bool moving;
 
-    private float moveInterval;
+    private float beatInterval;
     
     private float beatStartTime;
     private float lastIdleStart;
     private float lastFlashBeat;
     private float beatCounter;
     private float MoveSpeed;
+    private float beatDivider;
     void Start()
     {
-        moveInterval = 60 / bpm - idleInterval;
+        beatInterval = 60 / bpm;
         beatCounter = 0;
         MoveAngle= Random.Range(15,40);
+        beatDivider = 1 + Random.Range(0, 1);
+        AudioProcessor.Instance.onBeat.AddListener(onBeat);
     }
 
+    void onBeat()
+    {
+        MoveSpeed = Random.Range(0.5f, 2f);
+        //new set Start
+        beatStartTime = Time.time;
+        beatCounter++;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (lastFlashBeat != beatCounter)
-        {
-            var color = gameObject.GetComponentInChildren<SpriteRenderer>().color;
-            if (flash)
-            {
-                flash = false;
-                gameObject.GetComponentInChildren<SpriteRenderer>().color = 
-                    new Color(color.r, color.g, color.b, Random.Range(0.4f,0.6f));
-            }
-            else
-            {
-                if (Random.Range(0, 100) < 60)
-                {
-                    // time to flash
-                    gameObject.GetComponentInChildren<SpriteRenderer>().color = new Color(color.r, color.g, color.b, 0f);
-                    flash = true;
-                }
-            }
 
-            lastFlashBeat = beatCounter;
-        }
         
-        if (beatStartTime + moveInterval > Time.time)
+        if (beatCounter % beatDivider == 0)
         {
             // In Move
+             if (flash) 
+             {
+                 flash = false;
+                  var color = gameObject.GetComponentInChildren<SpriteRenderer>().color;
+                 gameObject.GetComponentInChildren<SpriteRenderer>().color = 
+                     new Color(color.r, color.g, color.b, Random.Range(0.4f,0.6f));
+             }
+            
             transform.rotation = Quaternion.Euler(0,0,MoveAngle/2 + MoveAngle/2 * Mathf.Cos(Time.time * MoveSpeed));
             
-        }else if (beatStartTime + moveInterval + idleInterval > Time.time)
-        {
+        }else{
             // In Idle
-            //do nothing
+            if (lastFlashBeat != beatCounter)
+            {
+                var color = gameObject.GetComponentInChildren<SpriteRenderer>().color;
+                if (flash)
+                {
+                    flash = false;
+                    gameObject.GetComponentInChildren<SpriteRenderer>().color = 
+                        new Color(color.r, color.g, color.b, Random.Range(0.4f,0.6f));
+                }
+                else
+                {
+                    if (Random.Range(0, 100) < 60)
+                    {
+                        // time to flash
+                        gameObject.GetComponentInChildren<SpriteRenderer>().color = new Color(color.r, color.g, color.b, 0f);
+                        flash = true;
+                    }
+                }
+                lastFlashBeat = beatCounter;
+            }
         }
-        else
-        {
-            MoveSpeed = Random.Range(1, 3);
-            //new set Start
-            beatStartTime = Time.time;
-            beatCounter++;
-        }
-        
+
     }
     
 }
