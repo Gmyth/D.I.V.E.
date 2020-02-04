47ca911c00020327105ab4d4ac842d1900ea5978
Gmyth <332087251@qq.com>
2020-02-04 05:03:54 -0500

    art fix

--- src/Cyberpunk2078/Assets/Scripts/VFX/LaserLight.cs
+++ src/Cyberpunk2078/Assets/Scripts/VFX/LaserLight.cs
@@ -7,16 +7,14 @@ public class LaserLight : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    private float MoveAngle;
-    private float bpm = 80;  //TODO: Audio
+    private float MoveAngle; 
     //private float idleInterval = Random.Range(0.05f,0.2f);
     
     private bool flash;
     private bool moving;
 
-    private float beatInterval;
     
-    private float beatStartTime;
+    private float acceptableBeat;
     private float lastIdleStart;
     private float lastFlashBeat;
     private float beatCounter;
@@ -24,37 +22,36 @@ public class LaserLight : MonoBehaviour
     private float beatDivider;
     void Start()
     {
-        beatInterval = 60 / bpm;
         beatCounter = 0;
         MoveAngle= Random.Range(15,40);
-        beatDivider = 1 + Random.Range(0, 1);
+        beatDivider = 2;
+        acceptableBeat = Random.Range(0, 1);
         AudioProcessor.Instance.onBeat.AddListener(onBeat);
     }
 
     void onBeat()
     {
         MoveSpeed = Random.Range(0.5f, 2f);
-        //new set Start
-        beatStartTime = Time.time;
         beatCounter++;
     }
     // Update is called once per frame
     void Update()
     {
-
         
-        if (beatCounter % beatDivider == 0)
+        if (beatCounter % beatDivider == acceptableBeat)
         {
             // In Move
              if (flash) 
              {
-                 flash = false;
-                  var color = gameObject.GetComponentInChildren<SpriteRenderer>().color;
-                 gameObject.GetComponentInChildren<SpriteRenderer>().color = 
-                     new Color(color.r, color.g, color.b, Random.Range(0.4f,0.6f));
+//                 flash = false;
+//                  var color = gameObject.GetComponentInChildren<SpriteRenderer>().color;
+//                 gameObject.GetComponentInChildren<SpriteRenderer>().color = 
+//                     new Color(color.r, color.g, color.b, Random.Range(0.4f,0.6f));
+                 
+                  transform.rotation = Quaternion.Euler(0,0,MoveAngle/2 + MoveAngle/2 * Mathf.Cos(Time.time * MoveSpeed));
              }
             
-            transform.rotation = Quaternion.Euler(0,0,MoveAngle/2 + MoveAngle/2 * Mathf.Cos(Time.time * MoveSpeed));
+           
             
         }else{
             // In Idle
