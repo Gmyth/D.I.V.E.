aa8bba6dfe6804729126c12ddec4539640839d2c
Haoyu Guo (RIT Student) <hg7364@ad.rit.edu>
2020-02-13 13:41:50 -0500

    finish art scene for level 3

--- src/Cyberpunk2078/Assets/Scripts/SimpleScripts/SimpleAlertDoor.cs
+++ src/Cyberpunk2078/Assets/Scripts/SimpleScripts/SimpleAlertDoor.cs
@@ -10,6 +10,7 @@ public class SimpleAlertDoor : MonoBehaviour
     public SimpleAlertEnemy[] enemies;
     private int enemyCount;
 
+    private bool unlocked;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +20,13 @@ public class SimpleAlertDoor : MonoBehaviour
         }
 
         enemyCount = enemies.Length;
+        GetComponent<Animator>().Play("KillDoor_Idle",0,-1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         
     }
 
@@ -39,9 +42,10 @@ public class SimpleAlertDoor : MonoBehaviour
 
     public void Unlock()
     {
-        gameObject.SetActive(false);
         //OpenedDoor.SetActive(true);
         //CameraManager.Instance.Shaking(0.6f, 1000000f);
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<Animator>().Play("KillDoor_Unlock",0,-1);
     }
 
 }
