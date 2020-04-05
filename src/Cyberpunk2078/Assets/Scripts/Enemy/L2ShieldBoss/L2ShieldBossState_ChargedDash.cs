a5ca47d7502d21fefdcdfe6dfc05a4d3fbfb7652
robinsetsuna <robinoneword@outlook.com>
2020-04-04 20:25:50 -0400

    fix reference

--- src/Cyberpunk2078/Assets/Scripts/Enemy/L2ShieldBoss/L2ShieldBossState_ChargedDash.cs
+++ src/Cyberpunk2078/Assets/Scripts/Enemy/L2ShieldBoss/L2ShieldBossState_ChargedDash.cs
@@ -4,4 +4,6 @@
 [CreateAssetMenuAttribute(fileName = "L2ShieldBossState_ChargedDash", menuName = "Enemy State/Level 2/Shield Boss/Charged Dash")]
 public class L2ShieldBossState_ChargedDash : ESChargedDash<L2ShieldBoss>
 {
+
+
 }
